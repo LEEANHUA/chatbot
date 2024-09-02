e000e47046a91b10019a5c3bc96e304e1f40a79b
宮本蓮 <miyamotoren@miyamotorennoMacBook-Air.local>
2024-09-02 15:58:30 +0900

    streamをオンオフするためにリファクタ

--- chat.py
+++ chat.py
@@ -10,17 +10,20 @@ def audio_play(wav_data):
     audio = AudioSegment.from_wav("audio/tmp.wav")
     play(audio)
 
-if __name__ == "__main__":
-    valid_stream = False
+def chat(valid_stream):
     llm = chatgpt.ChatGPT(valid_stream)
     while True:
         user_q = input("文章を入力：")
         
         llm_reault = llm.run_completion(user_q)
-        assistant_response = llm_reault.choices[0].message.content
-        llm.set_assistant_utterance(assistant_response)
-        
-        wav_data = voicevox.get_audio_file_from_text(assistant_response)
-        audio_play(wav_data)
-        
-        print(assistant_response)
+        if valid_stream == False:
+            assistant_response = llm_reault.choices[0].message.content
+            print(assistant_response)
+            llm.set_assistant_utterance(assistant_response)
+            
+            wav_data = voicevox.get_audio_file_from_text(assistant_response)
+            audio_play(wav_data)
+
+if __name__ == "__main__":
+    valid_stream = False
+    chat(valid_stream)
